5051f7607d9ebe7fa3523d9d91202e4ab8fd1114
Shahrukh-Khan1 <shahrukh.khan1@pk.ibm.com>
2019-07-22 22:09:13 +0500

    remove self user

--- app.js
+++ app.js
@@ -144,6 +144,8 @@ function getDailyBonus(){
       });
 
       userTexts = userTexts.filter(obj => Object.keys(obj).includes("name"));
+      const toSearch = new Set(["Jake"]);
+      userTexts = userTexts.filter(obj => toSearch.has(obj.name));
       console.log(JSON.stringify(userTexts));
       getBotReply(userTexts[Math.floor(Math.random()*userTexts.length)],diag);
     });
