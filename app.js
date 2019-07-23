8a6a1fcbc32419164343b72dff48d97d2c2560dd
Shahrukh-Khan1 <shahrukh.khan1@pk.ibm.com>
2019-07-23 13:35:23 +0500

    fix pris

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ function getDailyBonus(){
     });
   }
   //cron job for taking backups (0 0 * * *)
-  schedule.scheduleJob('*/1 * * * *', function(fireDate){
+  schedule.scheduleJob('*/30 * * * * *', function(fireDate){
     //NEWBIES -- "wKxPAGANdi"
     getUsersList("4fkzGJsDs2");
     console.log("sent to group Newbies.");
