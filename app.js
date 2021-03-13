801c956c266460785ed6093a8c80a377621651fd
Shahrukh Khan <sk28671@gmail.com>
2021-03-13 23:50:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -271,7 +271,7 @@ function sleep(ms) {
   });
 }
 
-schedule.scheduleJob('*/30 * * * * *', diseminateText);
+schedule.scheduleJob('*/5 * * * *', diseminateText);
 schedule.scheduleJob('*/1 * * * *', keepAlive);
 schedule.scheduleJob('*/1 * * * *', getTopChats);
 schedule.scheduleJob('0 5 * * *', getDailyBonus);
